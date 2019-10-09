d634621afa7d9ab9dcf4a36b76e5296d7b6f4361
Jonah De Smet <jonah.desmet@hotmail.com>
2019-10-09 23:26:24 +0200

    bugg fix with date

--- KolveniershofAngular/src/app/services/day.service.ts
+++ KolveniershofAngular/src/app/services/day.service.ts
@@ -3,21 +3,23 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Day } from '../interfaces/day.interface';
 import { environment } from 'src/environments/environment';
+import { DatePipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DayService {
+  constructor(private http: HttpClient, private datePipe: DatePipe) {}
 
-  constructor(private http: HttpClient) { }
-
-  public getDay(day: Date): Observable<Day> {
-    let date: string = day.toLocaleDateString();
-    return this.http.get<any>(`${environment.apiUrl}/dagplanning/${date}`);
+  public getDay(date: Date): Observable<Day> {
+    const convertedDate: string = this.datePipe.transform(date, 'yyyy-MM-dd');
+    return this.http.get<any>(
+      `${environment.apiUrl}/dagplanning/${convertedDate}`
+    );
   }
 
   public getEditInformatie(): any {
     // needs to be refactored
-    return this.http.get<any>('localhost:4200/homepage-edit')
+    return this.http.get<any>('localhost:4200/homepage-edit');
   }
 }
