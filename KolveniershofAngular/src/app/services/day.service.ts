f72445224de11982fb52a3591c391fdf58319910
Jonah De Smet <jonah.desmet@hotmail.com>
2019-10-09 09:10:03 +0200

    connection to backend

--- KolveniershofAngular/src/app/services/day.service.ts
+++ KolveniershofAngular/src/app/services/day.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Day } from '../interfaces/day.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,12 @@ export class DayService {
   constructor(private http: HttpClient) { }
 
   public getDay(day: Date): Observable<Day> {
-    return this.http.get<Day>(`localhost:4200/date`);
+    let date: string = day.toLocaleDateString();
+    return this.http.get<any>(`${environment.apiUrl}/dagplanning/${date}`);
   }
 
   public getEditInformatie(): any {
+    // needs to be refactored
     return this.http.get<any>('localhost:4200/homepage-edit')
   }
 }
