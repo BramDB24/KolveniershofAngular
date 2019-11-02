f06ed8b47538a2cfb39340ceba987fe92a69362d
Jonah De Smet <jonah.desmet@hotmail.com>
2019-11-02 19:08:09 +0100

    start van het login component

---
+++ KolveniershofAngular/src/app/services/account.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private readonly _tokenKey = 'currentUser';
+  private _user$: BehaviorSubject<string>;
+  public redirectUrl: string;
+
+  constructor(private http: HttpClient) {
+    let parsedToken = parseJwt(localStorage.getItem(this._tokenKey));
+    if (parsedToken) {
+      const expires =
+        new Date(parseInt(parsedToken.exp, 10) * 1000) < new Date();
+      if (expires) {
+        localStorage.removeItem(this._tokenKey);
+        parsedToken = null;
+      }
+    }
+    this._user$ = new BehaviorSubject<string>(
+      parsedToken && parsedToken.unique_name
+    );
+  }
+
+  public login(
+    loginCredential: string,
+    wachtwoord: string
+  ): Observable<boolean> {
+    return this.http
+      .post(
+        `${environment.apiUrl}/account`,
+        { loginCredential, wachtwoord },
+        { responseType: 'text' }
+      )
+      .pipe(
+        map((token: any) => {
+          if (token) {
+            localStorage.setItem(this._tokenKey, token);
+            this._user$.next(loginCredential);
+            return true;
+          } else {
+            return false;
+          }
+        })
+      );
+  }
+}
+
+function parseJwt(token) {
+  if (!token) {
+    return null;
+  }
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  return JSON.parse(window.atob(base64));
+}
