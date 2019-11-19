077500708210fa046ea4cc43873239ebd499b2ba
Jonah De Smet <jonah.desmet@student.hogent.be>
2019-11-19 21:38:35 +0100

    toevoegen van authorizatie (wip), backend error

--- KolveniershofAngular/src/app/navigation/navigation.component.ts
+++ KolveniershofAngular/src/app/navigation/navigation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AccountService } from '../services/account.service';
+import { Gebruiker } from '../models/gebruiker.model';
 
 @Component({
   selector: 'app-navigation',
@@ -8,11 +9,16 @@ import { AccountService } from '../services/account.service';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
-  public aangemelde = this.accountService.user;
+  public aangemelde: Gebruiker;
 
   constructor(private router: Router, private accountService: AccountService) { }
 
   ngOnInit() {
+    this.accountService.huidigeGebruiker.subscribe(t => this.aangemelde = t);
+  }
+
+  public isAdmin(): boolean {
+    return this.aangemelde && this.aangemelde.type === 'Begeleider';
   }
 
   public redirect(directory: string): void {
--- KolveniershofAngular/src/app/services/account.service.ts
+++ KolveniershofAngular/src/app/services/account.service.ts
@@ -3,68 +3,63 @@ import { Observable, BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
-
+import { Gebruiker } from '../models/gebruiker.model';
+import { constructor } from 'q';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class AccountService {
-  private readonly _tokenKey = 'currentUser';
-  public user: BehaviorSubject<string>;
-  public redirectUrl: string;
+    private readonly _tokenKey = 'currentUser';
+    public user: BehaviorSubject<Gebruiker>;
+    public huidigeGebruiker: Observable<Gebruiker>;
+    public redirectUrl: string;
 
-  constructor(private http: HttpClient) {
-    let parsedToken = parseJwt(localStorage.getItem(this._tokenKey));
-    if (parsedToken) {
-      const expires =
-        new Date(parseInt(parsedToken.exp, 10) * 1000) < new Date();
-      if (expires) {
-        localStorage.removeItem(this._tokenKey);
-        parsedToken = null;
-      }
+    constructor(private http: HttpClient) {
+        let parsedToken = parseJwt(localStorage.getItem(this._tokenKey));
+        if (parsedToken) {
+            const expires =
+                new Date(parseInt(parsedToken.exp, 10) * 1000) < new Date();
+            if (expires) {
+                localStorage.removeItem(this._tokenKey);
+                parsedToken = null;
+            }
+        }
+        this.user = new BehaviorSubject<Gebruiker>(
+            JSON.parse(localStorage.getItem('currentUser'))
+        );
+        this.huidigeGebruiker = this.user.asObservable();
     }
-    this.user = new BehaviorSubject<string>(
-      parsedToken && parsedToken.unique_name
-    );
-  }
 
-  public login(
-    email: string,
-    password: string
-  ): Observable<boolean> {
-    return this.http
-      .post(
-        `${environment.apiUrl}/account`,
-        { email,  password },
-        { responseType: 'text' }
-      )
-      .pipe(
-        map((token: any) => {
-          if (token) {
-            localStorage.setItem(this._tokenKey, token);
-            this.user.next(email);
-            return true;
-          } else {
-            return false;
-          }
-        })
-      );
-  }
-
-  public logout(): void {
-    if (this.user.getValue()) {
-      localStorage.removeItem('currentUser');
-      this.user.next(null);
+    public login(email: string, password: string): Observable<boolean> {
+        return this.http
+            .post(`${environment.apiUrl}/account`, { email, password })
+            .pipe(
+                map((token: any) => {
+                    if (token) {
+                        localStorage.setItem(this._tokenKey, JSON.stringify(token));
+                        this.user.next(token);
+                        return true;
+                    } else {
+                        return false;
+                    }
+                })
+            );
     }
-  }
 
+    public logout(): void {
+        if (this.user.getValue()) {
+            localStorage.removeItem('currentUser');
+            this.user.next(null);
+        }
+    }
 }
 
 function parseJwt(token) {
-  if (!token) {
-    return null;
-  }
-  const base64Url = token.split('.')[1];
-  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  return JSON.parse(window.atob(base64));
+    if (!token) {
+        return null;
+    }
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    return JSON.parse(window.atob(base64));
 }
