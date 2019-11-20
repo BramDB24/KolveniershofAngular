c1c620d6be9741d65b6cc6a5f8f38315bd8c3796
lucasverm <lucasvermeulen@gmail.com>
2019-11-20 19:52:24 +0100

    moving code to component + fixing layouts

--- KolveniershofAngular/src/app/dag/dag.component.ts
+++ KolveniershofAngular/src/app/dag/dag.component.ts
@@ -3,10 +3,6 @@ import { Component, Input, OnChanges } from "@angular/core";
 import { DagAtelier } from "../models/dag-atelier.model";
 import { DagPlanning } from "../models/dag-planning.model";
 import { DagService } from "../services/dag.service";
-import * as jsPDF from 'jspdf';
-import * as printJS from 'print-js';
-import html2canvas from 'html2canvas';
-
 
 @Component({
   selector: "app-dag",
@@ -54,47 +50,6 @@ export class DagComponent implements OnChanges {
       }
     );
   }
-
-  downloadPDF() {
-    /*let pdf = new jsPDF('p', 'mm', 'a4');
-  pdf.text = 
-  var pageHeight= pdf.internal.pageSize.height;
-  var data = document.getElementById('printVolledigeDag');  
-  html2canvas(data).then(canvas => {  
-  // Few necessary setting options  
-  var imgWidth = 208;   
-  var pageHeight = 295;    
-  var imgHeight = canvas.height * imgWidth / canvas.width;  
-  var heightLeft = imgHeight;  
-
-  const contentDataURL = canvas.toDataURL('image/png')  
-  var position = 0;  
-  pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
-  pdf.addPage()
-  
-  data = document.getElementById('printVoormiddag');  
-  html2canvas(data).then(canvas => {  
-  // Few necessary setting options  
-  var imgWidth = 208;   
-  var pageHeight = 295;    
-  var imgHeight = canvas.height * imgWidth / canvas.width;  
-  var heightLeft = imgHeight;  
-
-  const contentDataURL = canvas.toDataURL('image/png')  
-  var position = 0;  
-  pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
-  pdf.save('MYPdf.pdf'); // Generated PDF   
-  });  
-  });  
-*/
-    printJS({
-        printable: 'printVolledigeDag',
-        type: 'html',
-        honorColor: true,
-        targetStyles: ['*'],
-        ignoreElements: ['nietPrinten']
-    });
-}
 
   public haalDagplanningOpMetDatum(date: Date) {
     this.dagService.getDag(date).subscribe(
--- KolveniershofAngular/src/app/print/print.component.ts
+++ KolveniershofAngular/src/app/print/print.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as jsPDF from 'jspdf';
+import * as printJS from 'print-js';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-print',
@@ -11,5 +14,46 @@ export class PrintComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  downloadPDF() {
+    /*let pdf = new jsPDF('p', 'mm', 'a4');
+  pdf.text = 
+  var pageHeight= pdf.internal.pageSize.height;
+  var data = document.getElementById('printVolledigeDag');  
+  html2canvas(data).then(canvas => {  
+  // Few necessary setting options  
+  var imgWidth = 208;   
+  var pageHeight = 295;    
+  var imgHeight = canvas.height * imgWidth / canvas.width;  
+  var heightLeft = imgHeight;  
+
+  const contentDataURL = canvas.toDataURL('image/png')  
+  var position = 0;  
+  pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+  pdf.addPage()
+  
+  data = document.getElementById('printVoormiddag');  
+  html2canvas(data).then(canvas => {  
+  // Few necessary setting options  
+  var imgWidth = 208;   
+  var pageHeight = 295;    
+  var imgHeight = canvas.height * imgWidth / canvas.width;  
+  var heightLeft = imgHeight;  
+
+  const contentDataURL = canvas.toDataURL('image/png')  
+  var position = 0;  
+  pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+  pdf.save('MYPdf.pdf'); // Generated PDF   
+  });  
+  });  
+*/
+    printJS({
+        printable: 'print',
+        type: 'html',
+        honorColor: true,
+        targetStyles: ['*'],
+        ignoreElements: ['nietPrinten']
+    });
+}
 
 }
