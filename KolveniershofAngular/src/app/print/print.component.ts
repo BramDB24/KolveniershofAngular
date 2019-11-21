e822621a8290488d3bab6830e73e73121ebccd01
lucasverm <lucasvermeulen@gmail.com>
2019-11-21 11:37:45 +0100

    afwerken printing

--- KolveniershofAngular/src/app/print/print.component.ts
+++ KolveniershofAngular/src/app/print/print.component.ts
@@ -16,37 +16,6 @@ export class PrintComponent implements OnInit {
   }
 
   downloadPDF() {
-    /*let pdf = new jsPDF('p', 'mm', 'a4');
-  pdf.text = 
-  var pageHeight= pdf.internal.pageSize.height;
-  var data = document.getElementById('printVolledigeDag');  
-  html2canvas(data).then(canvas => {  
-  // Few necessary setting options  
-  var imgWidth = 208;   
-  var pageHeight = 295;    
-  var imgHeight = canvas.height * imgWidth / canvas.width;  
-  var heightLeft = imgHeight;  
-
-  const contentDataURL = canvas.toDataURL('image/png')  
-  var position = 0;  
-  pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
-  pdf.addPage()
-  
-  data = document.getElementById('printVoormiddag');  
-  html2canvas(data).then(canvas => {  
-  // Few necessary setting options  
-  var imgWidth = 208;   
-  var pageHeight = 295;    
-  var imgHeight = canvas.height * imgWidth / canvas.width;  
-  var heightLeft = imgHeight;  
-
-  const contentDataURL = canvas.toDataURL('image/png')  
-  var position = 0;  
-  pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
-  pdf.save('MYPdf.pdf'); // Generated PDF   
-  });  
-  });  
-*/
     printJS({
         printable: 'print',
         type: 'html',
