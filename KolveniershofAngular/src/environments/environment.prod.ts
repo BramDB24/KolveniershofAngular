c475cc9b4adb2d23231cd2d46c7e8db3f386a82d
Bram De Bleecker <bram.debleecker@student.hogent.be>
2019-12-20 02:38:30 +0100

    fix for production

--- KolveniershofAngular/src/environments/environment.prod.ts
+++ KolveniershofAngular/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   apiUrl: 'https://kolveniershof3punt2.azurewebsites.net/api',
+  imageUrl: 'https://kolveniershof3punt2.azurewebsites.net/pictos',
+  gebruikerUrl: 'https://kolveniershof3punt2.azurewebsites.net/gebruiker-foto',
   production: true
 };
