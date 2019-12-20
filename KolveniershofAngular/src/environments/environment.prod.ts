87eb9f3eee6fc94de887c6d0969dfcc40da73dde
Robin Vanluchene <robin.vanluchene@telenet.be>
2019-12-20 18:18:27 +0100

    environment terug naar origineel gezet

--- KolveniershofAngular/src/environments/environment.prod.ts
+++ KolveniershofAngular/src/environments/environment.prod.ts
@@ -1,9 +1,6 @@
 export const environment = {
-  // apiUrl: 'https://kolveniershof3punt2.azurewebsites.net/api',
-  // imageUrl: 'https://kolveniershof3punt2.azurewebsites.net/pictos',
-  // gebruikerUrl: 'https://kolveniershof3punt2.azurewebsites.net/gebruiker-foto',
-  apiUrl: '/api',
-  imageUrl: 'https://localhost:44339/pictos',
-  gebruikerUrl: 'https://localhost:44339/gebruiker-foto',
+  apiUrl: 'https://kolveniershof3punt2.azurewebsites.net/api',
+  imageUrl: 'https://kolveniershof3punt2.azurewebsites.net/pictos',
+  gebruikerUrl: 'https://kolveniershof3punt2.azurewebsites.net/gebruiker-foto',
   production: true
 };
--- KolveniershofAngular/src/environments/environment.ts
+++ KolveniershofAngular/src/environments/environment.ts
@@ -4,13 +4,11 @@
 
 export const environment = {
 
-  // apiUrl: 'https://kolveniershof3punt2.azurewebsites.net/api',
-  // imageUrl: 'https://kolveniershof3punt2.azurewebsites.net/pictos',
-  // gebruikerUrl: 'https://kolveniershof3punt2.azurewebsites.net/gebruiker-foto',
-  apiUrl: '/api',
-  imageUrl: 'https://localhost:44339/pictos',
-  gebruikerUrl: 'https://localhost:44339/gebruiker-foto',
-  
+  apiUrl: 'https://kolveniershof3punt2.azurewebsites.net/api',
+  imageUrl: 'https://kolveniershof3punt2.azurewebsites.net/pictos',
+  gebruikerUrl: 'https://kolveniershof3punt2.azurewebsites.net/gebruiker-foto',
+  // apiUrl: '/api',
+
   production: false
 };
 
